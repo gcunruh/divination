77ae73abfb823121a7bad3442514cfb803bf9d3b
gcunruh <49214225+gcunruh@users.noreply.github.com>
2022-04-03 21:09:19 -0500

    header init

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link";
 
 const tabs = [
     { name: 'Home', href: '/' },
@@ -8,16 +9,27 @@ const tabs = [
 
 import { useRouter } from "next/router";
 
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+function Pill({ text, href }) {
+    const router = useRouter();
+    const conditionalStyle = router.pathname == href ? "bg-white text-black" : "bg-transparent text-white"
+
+    return (
+        <Link href={href}>
+            <a className={"flex flex-row items-center mx-1 text-sm cursor-pointer rounded-full px-6 h-8 hover:opacity-60 transition ease-in-out delay-50 " + conditionalStyle}>
+            <div className="">
+                {text}    
+            </div>
+            </a>
+        </Link>
+    )
 }
 
 export default function Header() {
     const router = useRouter();
 
     return (
-        <>
-            <div className="my-3 mx-auto w-10/12 max-w-xl flex flex-row justify-center items-center">
+        <div className="my-3 md:my-4 flex flex-row justify-center md:justify-between w-full">
+            <div className="flex-row justify-center items-center hidden md:flex">
                 <Image
                     className="block w-auto"
                     src="/divination_logo.png"
@@ -26,35 +38,13 @@ export default function Header() {
                     width={36}
                     priority={true}
                 />
-                <text className="block text-white ml-3 font-mono tracking-wide">Divination</text>
+                <text className="text-white ml-3 font-mono tracking-wide">Divination</text>
             </div>
-            <div className="mx-auto w-11/12 md:w-1/2 lg:w-3/6 xl:w-5/12 my-5">
-                <div className="">
-                    <nav className="relative z-0 rounded-lg shadow flex divide-x divide-gray-200" aria-label="Tabs">
-                        {tabs.map((tab, tabIdx) => (
-                            <a
-                                key={tab.name}
-                                href={tab.href}
-                                className={classNames(
-                                    tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
-                                    tabIdx === 0 ? 'rounded-l-lg' : '',
-                                    tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
-                                    'group relative min-w-0 flex-1 overflow-hidden bg-white py-3 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10'
-                                )}
-                            >
-                                <span>{tab.name}</span>
-                                <span
-                                    aria-hidden="true"
-                                    className={classNames(
-                                        router.pathname == tab.href ? 'bg-tahiti' : 'bg-transparent',
-                                        'absolute inset-x-0 bottom-0 h-1'
-                                    )}
-                                />
-                            </a>
-                        ))}
-                    </nav>
-                </div>
+            <div className="flex flex-row rounded-full bg-black border-2 border-black h-10 my-4 bg-opacity-40 border-opacity-0 shadow-md items-center">
+                <Pill text="Home" href="/" />
+                <Pill text="Studio" href="/studio" />
+                <Pill text="Team" href="/about" />
             </div>
-        </>
+        </div>
     )
 }
